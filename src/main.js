2e7e8338c2b991a5f3f005c7340b859a9b4779f2
Niechcial Karol <niechcialkarol@gmail.com>
2024-11-11 12:47:08 +0100

    Clean default code, prepare to work

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
-import './assets/main.css'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import PrimeVue from "primevue/config";
+import Aura from "@primevue/themes/aura";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import App from "./App.vue";
+import router from "./router";
 
-import App from './App.vue'
-import router from './router'
+const app = createApp(App);
 
-const app = createApp(App)
+app.use(createPinia());
+app.use(router);
+app.use(PrimeVue, {
+	theme: {
+		preset: Aura,
+	},
+});
 
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+app.mount("#app");
