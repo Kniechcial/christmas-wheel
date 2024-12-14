b465864de4069feb9a42eaafc87286d2cd9cb647
Niechcial Karol <niechcialkarol@gmail.com>
2024-12-14 17:06:17 +0100

    Refactoring code, switch css on scss code

--- src/components/user_input/InputData.vue
+++ src/components/user_input/InputData.vue
@@ -1,115 +1,121 @@
 <template>
-	<div class="main-container">
-		<div class="content">
-			<!-- Kontener INPUT DATA -->
-			<div class="input-data blob">
-				<div class="flex flex-column">
+	<div class="wrapper">
+		<div class="layout-grid">
+			<!-- Formularz uczestników -->
+			<div class="participant-form">
+				<div class="flex-column">
 					<div>
-						<div class="input-description">Add participants to the draw</div>
-						<div class="card flex align-items-center mb-3">
-							<p class="input-font flex mr-3">Name:</p>
-							<InputText
-								label="Input Name"
-								type="text"
-								v-model="name" />
-						</div>
-						<div class="card flex align-items-center mb-3">
-							<p class="input-font flex mr-3">Email:</p>
-							<InputText
-								type="text"
-								v-model="email" />
-						</div>
-						<Button
-							label="Add Person"
-							severity="success"
-							:disabled="!canAddPerson"
-							@click="addPerson()"
-							rounded />
-						<p
-							v-if="persons.length === 16"
-							class="descriptionChangeText">
-							Maximum number of participants reached.
-						</p>
-					</div>
-				</div>
-			</div>
-			<div class="svg-line"></div>
-
-			<!-- Kontener YOUR INPUT -->
-			<div class="your-input">
-				<div class="display flex flex-column">
-					<div class="input-description">
-						People added to the Christmas draw
-					</div>
-					<div
-						class="input-description"
-						v-if="persons.length === 0">
-						Place for Persons playing in christmas gift
-						<div class="gift-image">
-							<img
-								src="../../../src/assets/image/gift.png"
-								alt="Gift Image" />
-						</div>
-					</div>
-					<div v-if="changeText"></div>
-					<div
-						class="descriptionChangeText"
-						:class="{ visible: changeText }">
-						{{ changeText || "\u00A0" }}
-					</div>
-					<ul>
-						<li
-							class="person"
-							v-for="(person, index) in persons.slice().reverse()"
-							:key="index">
-							<span class="person-name"
-								>{{ person.id }}. {{ person.name }}</span
-							>
-							<div class="selected-details">
-								<img
-									class="person-icon"
-									:src="person.icon"
-									alt="Person Icon"
-									@mouseover="handleChangeIconDescriptionDisplayOn(person)"
-									@mouseout="handleChangeIconDescriptionDisplayOut"
-									@click="changeIcon(person)" />
-								<div
-									class="person-colour"
-									:style="{ backgroundColor: person.color }"
-									alt="Person Color"
-									@mouseover="handleChangeColourDescriptionDisplayOn(person)"
-									@mouseout="handleChangeColourDescriptionDisplayOut"
-									@click="changeColor(person)"></div>
-								<button
-									class="delete-button"
-									@click="removePerson(person.id)">
-									<i class="pi pi-trash">X</i>
-								</button>
+						<div class="sticky-header">Add participants to the draw</div>
+						<div
+							class="card flex justify-content-between align-items-center gap-3">
+							<div class="card flex align-items-center mb-3 input-field">
+								<p class="label-font mr-3">Name:</p>
+								<InputText
+									label="Input Name"
+									type="text"
+									v-model="name" />
 							</div>
-						</li>
-					</ul>
+							<div class="card flex align-items-center mb-3 input-field">
+								<p class="label-font mr-3">Email:</p>
+								<InputText
+									type="text"
+									v-model="email" />
+							</div>
+						</div>
+						<div
+							class="card flex justify-content-between align-items-center input-field">
+							<Button
+								label="Add Person"
+								severity="success"
+								:disabled="!canAddPerson"
+								@click="addPerson()"
+								rounded />
+							<p
+								v-if="persons.length === 16"
+								class="invalid-description">
+								Maximum number of participants reached.
+							</p>
+						</div>
+					</div>
 				</div>
 			</div>
-			<!-- Kontener INPUT ADDITIONAL DATA -->
-			<div class="input-data-2 blob-2">
-				<div class="flex flex-column">
+
+			<!-- Separator -->
+			<div class="divider"></div>
+
+			<!-- Lista uczestników -->
+			<div class="participant-list">
+				<div class="sticky-header">People added to the Christmas draw</div>
+				<div v-if="persons.length === 0">
+					<div class="sticky-header">
+						Place for Persons playing in christmas gift
+					</div>
+					<div class="image-container">
+						<img
+							src="../../../src/assets/image/gift.png"
+							alt="Gift Image" />
+					</div>
+				</div>
+				<div
+					class="description"
+					v-if="changeText">
+					{{ changeText || "\u00A0" }}
+				</div>
+				<ul>
+					<li
+						class="list-item"
+						v-for="(person, index) in persons.slice().reverse()"
+						:key="index">
+						<span class="participant-name"
+							>{{ person.id }}. {{ person.name }}</span
+						>
+						<div class="actions">
+							<img
+								class="icon"
+								:src="person.icon"
+								alt="Person Icon"
+								@mouseover="handleChangeIconDescriptionDisplayOn(person)"
+								@mouseout="handleChangeIconDescriptionDisplayOut"
+								@click="changeIcon(person)" />
+							<div
+								class="color-preview"
+								:style="{ backgroundColor: person.color }"
+								alt="Person Color"
+								@mouseover="handleChangeColourDescriptionDisplayOn(person)"
+								@mouseout="handleChangeColourDescriptionDisplayOut"
+								@click="changeColor(person)"></div>
+							<button
+								class="delete-btn"
+								@click="removePerson(person.id)">
+								<i class="pi pi-trash">X</i>
+							</button>
+						</div>
+					</li>
+				</ul>
+			</div>
+
+			<!-- Formularz dodatkowych informacji -->
+			<div class="additional-info">
+				<div class="flex-column">
 					<div>
-						<div class="input-description">Add more information</div>
+						<div class="sticky-header">Add more information</div>
 						<div class="card flex align-items-center mb-3">
-							<p class="input-font flex mr-3">Date:</p>
+							<p class="label-font mr-3">Date:</p>
 							<InputText
+								class="input-field"
 								label="Input Name"
 								type="text"
 								v-model="userDate" />
 						</div>
 						<div class="card flex align-items-center mb-3">
-							<p class="input-font flex mr-3">Gift price:</p>
+							<p class="label-font mr-3">Gift price:</p>
 							<InputText
+								class="input-field"
 								type="number"
 								v-model="userPrice" />
 						</div>
 						<div class="card flex justify-center">
-							<div>Do you want add Your own welcome message?</div>
+							<div>Do you want to add your own welcome message?</div>
 							<div class="flex items-center gap-4">
 								<div class="flex items-center gap-2">
 									<RadioButton
@@ -135,7 +141,7 @@
 							</div>
 							<div
 								v-if="!addMessage"
-								class="defaultMessage">
+								class="default-message">
 								{{ dynamicMessage }}
 							</div>
 						</div>
@@ -151,6 +157,7 @@
 		</div>
 	</div>
 </template>
+
 <script setup>
 import { computed, ref } from "vue";
 import InputText from "primevue/inputtext";
@@ -195,8 +202,8 @@ const availableColors = ref([
 ]);
 
 const dynamicMessage = computed(() => {
-	const date = userDate.value || "12.12";
-	const price = userPrice.value || "100";
+	const date = userDate.value || "24.12";
+	const price = userPrice.value || "150";
 	return `Hello! Christmas is fast approaching, so we have prepared a draw. I hope you will be happy with the people you will draw to whom you will give a Christmas present. We will give gifts for ${date}, and the minimum amount we will spend on a gift is ${price} pln. Have fun and see you soon :)`;
 });
 const setWelcomeMessage = () => {
@@ -299,204 +306,186 @@ const isButtonEnabled = computed(() => {
 	return userDate.value.trim().length > 0 && userPrice.value.trim().length > 0;
 });
 </script>
-<style>
-.main-container {
+<style style lang="scss">
+// Zmienne dla powtarzających się wartości
+$font-primary: "Imperial Script", cursive;
+$bg-color: #f8f9fa;
+$border-color: black;
+$shadow: 0px 4px 6px rgba(0, 0, 0, 0.2);
+$highlight-color: #f44336;
+
+// Mixin dla powtarzających się stylów
+@mixin flex-center($direction: row) {
 	display: flex;
-	flex-direction: column;
+	flex-direction: $direction;
 	align-items: center;
+}
+
+@mixin blob-shape($radius: 54% 46% 36% 64% / 44% 61% 39% 56%) {
+	border-radius: $radius;
+	box-shadow: $shadow;
+	overflow: hidden;
+	background: $bg-color;
+}
+
+.wrapper {
+	@include flex-center(column);
 	gap: 60px;
-}
-.description {
-	text-align: center;
-}
 
-.content {
-	display: grid;
-	width: 100%;
-	grid-template-columns: 1fr auto 1fr;
-	grid-template-rows: auto;
-	column-gap: 50px;
-	row-gap: 20px;
-	border: solid 2px green;
-}
+	.sticky-header {
+		position: sticky;
+		top: 0;
+		z-index: 1;
+		font-family: $font-primary;
+		font-size: 40px;
+		margin-bottom: 0.5rem;
+	}
 
-.input-data {
-	margin-left: 1rem;
-	grid-column: 1;
-	height: 30rem;
-	width: 40rem;
-	border: solid 2px black;
-}
-.input-data-2 {
-	margin-left: 1rem;
-	grid-column: 1;
-	grid-row: 3;
-	height: 35rem;
-	width: 40rem;
-	border: solid 2px black;
-}
-.input-description {
-	position: sticky;
-	top: 0;
-	z-index: 1;
-	background: #f8f9fa;
-	font-family: "Imperial Script", cursive;
-	font-size: 40px;
-	font-weight: 400;
-	/* padding: 0.5rem; */
-	margin-bottom: 0.5rem;
-}
-.input-font {
-	font-family: "Imperial Script", cursive;
-	font-size: 26px;
-	width: 80px;
-	text-align: right;
-}
+	.layout-grid {
+		display: grid;
+		width: 100%;
+		grid-template-columns: 1fr auto 1fr;
+		gap: 20px 50px;
+		border: 2px solid green;
 
-.your-input {
-	/* transform: translateY(-5rem); */
-	height: 30rem;
-	width: 40rem;
-	grid-column: 3;
-	grid-row: 2;
-	border-radius: 54% 46% 36% 64% / 44% 61% 39% 56%;
-	align-items: center;
-	overflow: hidden;
-	padding: 2rem;
-	background: #f8f9fa;
-	display: flex;
-	flex-direction: column;
-	padding: 6rem;
-	box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.2);
-}
-.svg-line {
-	grid-column: 2;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	border: solid 2px black;
-}
+		.participant-form,
+		.additional-info {
+			@include blob-shape();
+			padding: 6rem;
+			width: 40rem;
+			margin-left: 2rem;
 
-.confirm {
-	text-align: center;
-}
-ul {
-	max-height: 11rem;
-	overflow-y: scroll;
-	scrollbar-width: none;
-	-ms-overflow-style: none;
-	list-style: none;
-	padding: 0 2rem;
-	margin: 0;
-	width: 100%;
-}
-ul::-webkit-scrollbar {
-	display: none;
-}
+			&.additional-info {
+				height: 35rem;
+				margin-top: -2rem;
+				border-radius: 55% 54% 76% 36% / 71% 44% 69% 45%;
+			}
+			&.participant-form {
+				margin-top: 2rem;
+			}
 
-.blob {
-	display: flex;
-	padding: 6rem;
-	border-radius: 46% 54% 64% 36% / 61% 44% 56% 39%;
-	align-items: center;
-	overflow: hidden;
-	background: #f8f9fa;
-	box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.2);
-}
-.blob-2 {
-	display: flex;
-	padding: 6rem;
-	border-radius: 55% 54% 76% 36% / 71% 44% 69% 45%;
-	align-items: center;
-	overflow: hidden;
-	background: #f8f9fa;
-	box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.2);
-}
+			.label-font {
+				font-family: $font-primary;
+				font-size: 26px;
+				text-align: right;
+			}
+			.input-field {
+				flex-direction: column;
+				max-width: 400px;
+				box-sizing: border-box;
+			}
+		}
+		.additional-info {
+			grid-row: 3;
+			grid-column: 1;
+		}
 
-.li {
-	border: solid 2px black;
-}
-.person {
-	display: grid;
-	grid-template-columns: 60% 40%;
-	padding: 0.5rem 0;
-	align-items: center;
-	border-bottom: 1px solid #ddd;
-}
+		.participant-list {
+			@include blob-shape();
+			padding: 6rem;
+			width: 40rem;
+			margin-right: 1rem;
+			height: 30rem;
+			grid-column: 3;
+			grid-row: 2;
+			display: flex;
+			flex-direction: column;
+			font-family: $font-primary;
 
-.person-name {
-	text-align: left;
-	padding: 0.5rem;
-	font-family: "Imperial Script", cursive;
-	font-size: 22px;
-	margin: 0;
-}
-.selected-details {
-	display: flex;
-	padding: 0.5rem;
-	align-items: center;
-	justify-content: flex-start;
-}
+			ul {
+				max-height: 11rem;
+				overflow-y: scroll;
+				padding: 0 2rem;
+				margin: 0;
+				list-style: none;
 
-.person-icon {
-	width: 35px;
-	height: 35px;
-	cursor: pointer;
-	margin-right: 0.5rem;
-}
+				&::-webkit-scrollbar {
+					display: none;
+				}
 
-.person-colour {
-	width: 50px;
-	height: 35px;
-	border-radius: 15%;
-	cursor: pointer;
-}
-.descriptionChangeText {
-	color: red;
-	max-width: 40rem;
-	font-family: "Imperial Script", cursive;
-	font-size: 20px;
-	font-weight: 500;
-	padding-right: 3rem;
-	text-align: right;
-}
+				.list-item {
+					display: grid;
+					grid-template-columns: 60% 40%;
+					padding: 0.5rem 0;
+					align-items: center;
+					border-bottom: 1px solid #ddd;
 
-.delete-button {
-	background: none;
-	border: none;
+					.participant-name {
+						text-align: left;
+						font-family: $font-primary;
+						font-size: 22px;
+						margin: 0;
+					}
 
-	font-weight: 800;
-	color: #f44336;
-	cursor: pointer;
-	width: 50px;
-	height: 35px;
-}
+					.actions {
+						@include flex-center();
+						justify-content: flex-start;
 
-.delete-button:hover {
-	color: #d32f2f;
-	scale: 1.2;
-}
-.gift-image {
-	position: absolute;
-	right: 0;
-	width: 10rem;
-	height: 10rem;
-	z-index: 0;
-	overflow: hidden;
-}
+						.icon,
+						.color-preview {
+							width: 35px;
+							height: 35px;
+							cursor: pointer;
+							margin-right: 0.5rem;
+						}
 
-.gift-image img {
-	width: 100%;
-	height: 100%;
-	object-fit: cover;
-	opacity: 0.8;
-}
-.defaultMessage {
-	font-family: "Imperial Script", cursive;
-	padding-top: 1rem;
-	padding-bottom: 0.5rem;
-	font-size: 20px;
-	width: 400px;
-	height: 140px;
+						.color-preview {
+							border-radius: 15%;
+						}
+
+						.delete-btn {
+							background: none;
+							border: none;
+							font-weight: 800;
+							color: $highlight-color;
+							cursor: pointer;
+							width: 50px;
+							height: 35px;
+
+							&:hover {
+								color: black;
+								transform: scale(1.2);
+							}
+						}
+					}
+				}
+			}
+		}
+
+		.divider {
+			grid-column: 2;
+			@include flex-center();
+			border: 2px solid $border-color;
+		}
+	}
+
+	.image-container {
+		position: relative;
+		width: 10rem;
+		height: 10rem;
+		overflow: hidden;
+		float: right;
+
+		img {
+			width: 100%;
+			height: 100%;
+			object-fit: cover;
+			opacity: 0.8;
+		}
+	}
+
+	.default-message {
+		font-family: $font-primary;
+		font-size: 20px;
+		width: 400px;
+		height: 140px;
+		padding: 1rem 0 0.5rem;
+	}
+	.invalid-description {
+		font-family: $font-primary;
+		font-size: 20px;
+		padding-top: 1rem;
+		color: red;
+	}
 }
 </style>
